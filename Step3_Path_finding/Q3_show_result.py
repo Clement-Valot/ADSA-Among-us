8b9fda9af9d65d591a24b2d21071d4379c481eaf
Clement Mutez <mutezclement@gmail.com>
2020-12-11 19:38:54 +0100

    Add commment on main, Q3 and Q4

--- Step3_Path_finding/Q3_show_result.py
+++ Step3_Path_finding/Q3_show_result.py
@@ -1,5 +1,14 @@
 from Q2_dijkstra import *
 
+'''
+#* Function use in all time to verify a condition
+
+#? result : tuple composed of (first_node, last_node, cost)
+#? list_of_all_result : list that contain all the result that we already visit
+
+#? return the condition
+
+'''
 def Condition_for_all_time(result, list_of_all_result):
     condition = True
     # We verify if it is not the first node coupled with the firts node
@@ -17,6 +26,14 @@ def Condition_for_all_time(result, list_of_all_result):
     
     return condition
 
+
+'''
+#* Function that allows to show the time to travel for any pair of rooms for a graph
+
+#? graph : crew mate graph or impostor graph
+
+#? return the list of the result
+'''
 def all_time(graph):
     list_final = []
     list_of_all_result = []
@@ -34,12 +51,21 @@ def all_time(graph):
         for node_final, value in dict_of_result.items():
             # We create a tuple of the result
             result = ((start_node, node_final, value))
+            # We verify the condition (see below to see all the condition)
             condition = Condition_for_all_time(result, list_of_all_result)
             if condition:
+                # We add the result to the final list
                 list_final.append(result)
+            # Even if the result is not good we add it to be sure it will not be seen another time
             list_of_all_result.append(result)
     return list_final
-    
+
+
+'''
+#* Function that allows us to print the result from the previous function
+
+#? list_of_result : list of all the result calculted in the previous function
+'''
 def print_the_result(list_of_result):
     for i in range(len(list_of_result)):
         print("The connexion betweem room {} and room {} takes {} second \n".format(list_of_result[i][0], 
--- Step3_Path_finding/main.py
+++ Step3_Path_finding/main.py
@@ -3,7 +3,10 @@ from Q2_dijkstra import *
 from Q3_show_result import *
 from Q4_compare import *
 
-
+'''
+#* Function to execute test for question 2
+#* You can compute the dijkstra alorithm on different model with different source node
+'''
 def Question2():
     graph_crew_mate = Create_map_crewmate()
     graph_impostor = Create_map_impostor()
@@ -16,24 +19,40 @@ def Question2():
     resultat3 = dijsktra(graph_impostor,"UE")
     print(resultat3)
     
-
+'''
+#* Function to execute to test the question 3
+#* This function will show you the time travel for any pair of room for both model
+'''
 def Question3():
+    # We create the 2 graph
     graph_crew_mate = Create_map_crewmate()
     graph_impostor = Create_map_impostor()
 
+    # We create the list of result for the crew mate
     list_of_result_crewmate = all_time(graph_crew_mate)
+    print("Here are the result for the crew mate :\n")
+    # We print the result
     print_the_result(list_of_result_crewmate)
     print("The lenght is : {}".format(len(list_of_result_crewmate)))
 
+    # We create the list of result for the impostor
     list_of_result_impostor = all_time(graph_impostor)
+    print("Here are the result for the impostor :\n")
+    # We print the result
     print_the_result(list_of_result_impostor)
     print("The lenght is : {}".format(len(list_of_result_impostor)))
 
+'''
+#* Function to execute to test question 3
+#* This function compare the time travel between the two model for any paif of room
+'''
 
 def Question4():
+    # We create the 2 graph
     graph_crew_mate = Create_map_crewmate()
     graph_impostor = Create_map_impostor()
 
+    # We do the comparison
     compare_both_model(graph_crew_mate, graph_impostor)
 
 
