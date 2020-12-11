d502a1fe05afd88d7e9b583eabc5dbffbcb5eaaa
Clement Mutez <mutezclement@gmail.com>
2020-12-11 18:41:43 +0100

    stage 3 : implement method to show the result for any pair of rooms for both model

--- Step3_Path_finding/main.py
+++ Step3_Path_finding/main.py
@@ -87,11 +87,56 @@ def dijsktra(graph, initial_node):
     
     return visited
 
+def Condition_for_all_time(graph, result, list_of_all_result):
+    condition = True
+    # We verify if it is not the first node coupled with the firts node
+    if result[0] == result[1]:
+        condition = False
+    # We verify if we already have the result but in an other way (start_node <-> end_node)
+    # and if the room is "COR" which doesn't interst us
+    for i in range (len(list_of_all_result)):
+        if result[0] == list_of_all_result[i][0] and result[1] == list_of_all_result[i][1]:
+            condition = False
+        if result[1] == list_of_all_result[i][0] and result[0] == list_of_all_result[i][1]:
+            condition = False
+        if result[1] == "COR" or result[0] == "COR":
+            condition = False
+    
+    return condition
+
+def all_time(graph):
+    list_final = []
+    list_of_all_result = []
+    list_of_node = []
+    dict_of_result = {}
+    condition = False
+    # Creation of the list that contain all the node
+    for key in graph.dico_nodes.keys():
+        list_of_node.append(key)
+    # For every node in the list, we run dijkstra
+    for start_node in list_of_node:
+        # We store the result
+        dict_of_result = dijsktra(graph, start_node)
+        # For every result
+        for node_final, value in dict_of_result.items():
+            # We create a tuple of the result
+            result = ((start_node, node_final, value))
+            condition = Condition_for_all_time(graph, result, list_of_all_result)
+            if condition:
+                list_final.append(result)
+            list_of_all_result.append(result)
+    return list_final
+    
+def print_the_result(list_of_result):
+    for i in range(len(list_of_result)):
+        print("The connexion betweem room {} and room {} takes {} second \n".format(list_of_result[i][0], 
+        list_of_result[i][1], list_of_result[i][2]))
 
 if __name__ == "__main__":
+    
     graph_crew_mate = Create_map_crewmate()
     graph_impostor = Create_map_impostor()
-
+    '''
     resultat = dijsktra(graph_crew_mate,"RE")
     print(resultat)
     resultat2 = dijsktra(graph_crew_mate,"RE")
@@ -99,3 +144,12 @@ if __name__ == "__main__":
 
     resultat3 = dijsktra(graph_impostor,"RE")
     print(resultat3)
+    '''
+
+    list_of_result_crewmate = all_time(graph_crew_mate)
+    print_the_result(list_of_result_crewmate)
+    print("The lenght is : {}".format(len(list_of_result_crewmate)))
+
+    list_of_result_impostor = all_time(graph_impostor)
+    print_the_result(list_of_result_impostor)
+    print("The lenght is : {}".format(len(list_of_result_impostor)))
