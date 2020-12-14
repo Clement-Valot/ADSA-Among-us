7a71e4e2cae50d9fe07aa5a846ae69404f115f6c
Clement Valot <val.croissy@gmail.com>
2020-12-15 00:46:08 +0100

    Step2 complete et tres efficace, summed_adjacency_matrix tellement efficace qu'elle empeche les autres méthodes de tester leur efficacité (impostorness coefficient, exoneration), il faudrait donc encore faire des tests avec d'autres situations pour les tester.

--- Step2 Find Impostors by sight/Player.py
+++ Step2 Find Impostors by sight/Player.py
@@ -2,34 +2,50 @@ class Player:
     def __init__(self, ID):
         self.ID = ID
         self.alive=True
+        self.innocent=False
         self.imp_coeff=0
         self.players_seen_by_round=[]
-        self.round_coeff=[]
+        self.round_coeff=[0]
 
-def Get_players_alive(list_players):
+def Get_Seen_Alive(players, players_seen):
     alives=[]
-    for player in list_players:
-        if player.alive:
+    for player in players:
+        if((player.alive) and (player.ID in players_seen)):
             alives.append(player)
     return alives
 
-def Get_list_ID_players(list_players):
+def Get_Seen_Unexorated(players, players_seen):
+    seen_unexonerated=[]
+    for player in players:
+        if((not player.innocent) and (player.ID in players_seen)):
+            seen_unexonerated.append(player)
+    return seen_unexonerated
+
+def Get_list_ID_players(players):
     IDs=[]
-    for player in list_players:
+    for player in players:
         IDs.append(player.ID)
     return IDs
 
-def Get_Player_From_ID(ID, list_players_alive):
-    for player in list_players_alive:
+def Get_Player_From_ID(players, ID):
+    for player in  players:
         if ID==player.ID:
             return player
-    
-def Get_Best_Suspect(list_players_alive):
+
+def Remove_Impostors(players, impostors):
+    if type(impostors)==list:
+        for impostor in impostors:
+            if impostor in players:
+                players.remove(impostor)
+    elif type(impostors)==int:
+        if impostors in players:
+            players.remove(impostors)
+
+def Get_Best_Suspect(players):
     coeff=0
     best_suspect=""
-    for player in list_players_alive:
+    for player in players:
         if player.imp_coeff>coeff:
             best_suspect=player
             coeff=player.imp_coeff
     return best_suspect
-
--- Step2 Find Impostors by sight/main.py
+++ Step2 Find Impostors by sight/main.py
@@ -1,4 +1,68 @@
+from Player import *
+from Game_functions import *
 
+players_alive=[]
+for i in range(10):
+    players_alive.append(Player(i)) 
 
-list_players=[1,2,3,4,5,6,7,8,9,0]
-list_players_alive=[1,2,3,4,5,6,7,8,9,0]
+Impostors=[1,7] 
+
+summed_adjacency_matrix=np.zeros((10,10))
+
+Game_ended=False
+Crewmates_won=True 
+
+round=-1
+while(not Game_ended):
+    round+=1
+    #We randomly create the Adjacency matrix 
+    new_adjacency_matrix= Create_Adjacency_Matrix(players_alive, Impostors)   
+    print("\n\nHere is the adjacency matrix of round {}:\n{}".format(11-len(players_alive),new_adjacency_matrix))
+    #The summed adjacency matrix is useful to see how many times each players saw each other
+    # but most of all to highlight if players didn't see each other at all during the game.
+    # Indeed if we have players that saw each other 3 to 4 times and others 0, those players have
+    # a higher probability to be impostors. 
+    # However, this technic is only useful when 2 impostors are remaining.
+    if(len(Impostors)==2):
+        summed_adjacency_matrix+=new_adjacency_matrix
+        print("Here is the summed adjacency matrix of round {}:\n{}".format(11-len(players_alive),summed_adjacency_matrix))
+        impostors=Check_impostor(players_alive, summed_adjacency_matrix)
+        if(len(impostors)!=0):
+            for impostor in impostors:
+                Impostors.remove(impostor.ID) 
+                players_alive.remove(impostor)
+                print("Player {} was eliminated by crewmates because he was an impostor".format(impostor.ID))
+            print("\nBusted by the sum of Adjacency Matrix")
+            break
+            
+    #Now we kill one crewmate; we remove this player from  players_alive and we 
+    #return the list of suspects for this kill, i.e. the previously updated attribute of seen players. 
+    #Plus, we update all the Impostorness coefficients. 
+    players_alive, Impostors=Kill_Crewmate(players_alive, Impostors)
+    #If we arrive in a critical situation where if we don't unmask the impostor this round
+    #they will win by numerical superiority after their next kill, we have to make a decision.
+    # And this is where the Impostorness coefficient plays its role.
+    if (len(players_alive)-len(Impostors)<=1): 
+        Most_probable_impostor=Get_Best_Suspect(players_alive) 
+        players_alive.remove(Most_probable_impostor) 
+
+        #If the player we decided to kill wasn’t the impostor, then we couldn’t get past 
+        # the critical situation and impostors won. Nonetheless, if we succeed to kill 
+        # one impostor, then either the game ends and crewmates win since there are no impostors 
+        # remaining, or the game continues, and we loop once again. 
+        if(Most_probable_impostor not in Impostors): 
+            print("{} was eliminated but he was not an impostor!".format(Most_probable_impostor.ID))
+            Game_ended=True 
+            Crewmates_won=False   
+        else:
+            print("{} was eliminated and he was an impostor!".format(Most_probable_impostor.ID))   
+
+    if(len(Impostors)==0):
+        Game_ended=True    
+
+#After the game ends, we display a winning message 
+
+if(Crewmates_won): 
+    print("\n\nCongratulations to crewmates, you found and eliminated all impostors!\n\n") 
+else: 
+    print("\n\nImpostors {} got the best of you and killed everyone!\n\n".format(Impostors)) 
