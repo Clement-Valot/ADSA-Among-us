356ae89f0f02d2baae662c64da5e3db6f0582b44
Clement Mutez <mutezclement@gmail.com>
2020-12-15 10:35:24 +0100

    Add comments, function for question 4

--- Step4_Secure_Last_task/GraphClass.py
+++ Step4_Secure_Last_task/GraphClass.py
@@ -6,62 +6,107 @@ class Graph:
 
   '''
   # To initialize a grah with a dictionnary of node and list of edges
-  #? dico_node : dictionnary that store node in the form : dict[name_of_the_node] = value
-  #? list_edges : list that store edges in the form : list[(start_node, end_node, cost), (start_node, end_node, cost), ...]
+  #? number_of_node : number of the node in the graph
+  #? N : number of the node in the graph
+  #? graph : data frame that is going to store the adjacency matrix
   '''
   def __init__(self, number_of_nodes): 
     self.graph = pd.DataFrame()
     self.N = number_of_nodes
 
+  '''
+  #* Function use in Hamiltonian to verify if the node is good : 
+  #* -not already in the path
+  #* -node is adjacent of the previous node 
 
+  #? n : node to verify, it correspond to a line in the adjacency matrix
+  #? pos : position in the path
+  #? path : Hamiltonian path, with all the node already in the path
 
+  #? return True if the node is good
+  #? return False otherwise
+  '''
   def isGood(self, n, pos, path):
+    # Verify if the node is adjacent to the previous node in the adjacency matrix
     if self.graph.iloc[path[pos-1],n] == 0:
       return False
+    # Verify if the node is already in the path 
     for node in path:
       if node == n:
         return False
     return True
   
-  def hamCycleUtil(self, path, pos):
-    # base case: if all vertices are  
-    # included in the path  
+  '''
+  #* Function that finds the Hamiltonian path from an original node
+  #* The original node is put in parameter in Ham_Solution
+
+  #? path : store the Hamiltonian path
+  #? pos : store the position (the state of the recursive function)
+  '''
+  def Hamiltonian(self, path, pos):
+    # if all vertices are included in the path we stop the recursion and return true
     if pos == self.N: 
       return True
   
-    # Try different vertices as a next candidate  
-    # in Hamiltonian Cycle. We don't try for 0 as  
-    # we included 0 as starting point in hamCycle()  
-    for v in range(0,self.N):  
-      if self.isGood(v, pos, path) == True:  
-        path[pos] = v  
-        if self.hamCycleUtil(path, pos+1) == True:  
+    # Try different vertices as a next candidate in Hamiltonian path
+    # So we travel through each node (meaning each line of the adjacency matrix)
+    for node in range(0,self.N): 
+      # We verify that the node is good (not already in the path, previous node adjacent to the current node) 
+      # The node is represented by his line number in the adjacency matrix
+      if self.isGood(node, pos, path) == True:  
+        # If the node is good we put it in the path
+        path[pos] = node  
+        # Recursion to find the Hamiltonian path
+        if self.Hamiltonian(path, pos+1) == True:  
           return True
   
-        # Remove current vertex if it doesn't  
-        # lead to a solution  
+        # Remove the current node if it doesn't lead to a solution  
         path[pos] = -1
+    # If it never return True we return False
     return False
   
-  def hamCycle(self):  
+
+  '''
+  #* Function to lunch the search for Hamiltonian path
+
+  #? starting_node : node where we want to start the Hamiltonian path
+
+  #? return True and print the solution if there is a Hamiltonian path
+  #? retunr False and print an error message if there is no solution
+  '''
+  def Ham_Solution(self, starting_node): 
+    # We initialized the list that will store the path 
+    # It has the size of the number of node and we initialize to -1 each node of the path 
     path = [-1] * self.N  
-  
-    ''' Let us put vertex 0 as the first vertex  
-    in the path. If there is a Hamiltonian Cycle,  
-    then the path can be started from any point  
-     of the cycle as the graph is undirected '''
-    path[0] = 11
-  
-    if self.hamCycleUtil(path,1) == False:  
-      print ("Solution does not exist\n") 
+
+    # We initialize the first node of the path that will be used to lunch the Hamiltonian function
+    path[0] = starting_node
+
+    list_room =  ["ME", "AD", "UE", "RE", "SE", "LE", "EL", "ST", "CO", "SH", "O2", "WE", "NA", "CA"]
+
+    # We lunch the Hamiltonian function with the path that we just initialize,
+    # and a position of 1 (because one node is already in the path)
+    if self.Hamiltonian(path,1) == False:
+      # If the result is false it means there is no Hamiltonian path from the starting node  
+      print ("Solution does not exist for room : {}\n".format(list_room[starting_node])) 
       return False
-  
-    self.printSolution(path)  
+
+    # If we didn't return false previously, it means the Hamiltonian path exist
+    # So we use the print function to print the path
+    self.printSolution(path, starting_node)  
     return True
 
-  def printSolution(self, path):  
-    print ("Solution Exists: Following", 
-            "is one Hamiltonian Cycle")
+  '''
+  #* Function that print the solution once the Hamiltonian path is found
+
+  #? path : Hamiltonian path found previously composed of node (indicated by their index in the adjacency matrix)
+  '''
+  def printSolution(self, path,original_node):  
+    # To print the solution we need to have the index of each room in the adjacency matrix
+    # To do so we recover the list of room use in the creation of the map in 'Create_map_function
     list_room =  ["ME", "AD", "UE", "RE", "SE", "LE", "EL", "ST", "CO", "SH", "O2", "WE", "NA", "CA"] 
+    print ("One Hamiltonian path has been found from the original node : {}".format(list_room[original_node]))
+    # The path list is composed of node represented by their index in the adjacency matrix
+    # So for each node (index) we print it in thanks to its corresponding room contain in list_room
     for vertex in path:
       print (list_room[vertex] , " ") 
--- Step4_Secure_Last_task/main.py
+++ Step4_Secure_Last_task/main.py
@@ -1,14 +1,29 @@
 from Create_map_function import *
 from GraphClass import *
 
+def adjecent_room(graph, room):
+    list_room = graph.graph[room]
+    print("list room : {}".format(list_room))
+    final_list =[]
+    for i in range(0, len(list_room)):
+        if list_room[i] != 0:
+            final_list.append(i)
+    print("final list {}".format(final_list))
+    return final_list
+
+
+
 '''
 #* Function to execute test for question 2
 #* You can compute the dijkstra alorithm on different model with different source node
 '''
-def Question2():
+def Question4():
     graph_crew_mate = Create_map_crewmate()
-    graph_crew_mate.hamCycle()
+    list_adjacent_room = adjecent_room(graph_crew_mate, "CA")
+    for node in list_adjacent_room:
+        graph_crew_mate.Ham_Solution(node)
+    
     
     
 if __name__ == "__main__":
-    Question2()
+    Question4()
